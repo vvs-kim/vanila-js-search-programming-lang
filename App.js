55f7fd363d564dcb6c4a17683887fe045bd03ae0
linkrws <kws60000@naver.com>
2022-11-04 16:06:18 +0900

    feat: 프로그래밍 언어 검색 시 제안목록 출력

---
+++ App.js
@@ -0,0 +1,46 @@
+import SearchInput from './SearchInput.js';
+import Suggestion from './Suggestion.js';
+
+import { fetchLanguages } from './api.js';
+
+export default function App({ $target }) {
+    this.state = {
+        fetchedLanguages: [],
+        selectedLanguaches: [],
+    };
+
+    this.setState = (nextState) => {
+        this.state = {
+            ...this.state,
+            ...nextState,
+        };
+
+        suggestion.setState({
+            items: this.state.fetchedLanguages,
+        });
+    };
+
+    const searchInput = new SearchInput({
+        $target,
+        initialState: '',
+        onChange: async (keyword) => {
+            if (keyword.length === 0) {
+                this.setState({
+                    fetchedLanguages: [],
+                });
+            } else {
+                const languages = await fetchLanguages(keyword);
+                this.setState({
+                    fetchedLanguages: languages,
+                });
+            }
+        },
+    });
+
+    const suggestion = new Suggestion({
+        $target,
+        initialState: {
+            items: [],
+        },
+    });
+}
